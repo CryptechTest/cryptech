ab8dc1763ceb7d30ff471b431f4f31e4da40a494
SeqSEE <SeqSEE@users.noreply.github.com>
2023-04-16 03:15:05 -0400

    add infotext to beds

--- mods/beds/api.lua
+++ mods/beds/api.lua
@@ -136,6 +136,8 @@ function beds.register_bed(name, def)
 				minetest.after(0, function()
 					unified_inventory.set_inventory_formspec(placer, unified_inventory.current_page[player_name])
 				end)
+				local node_meta = minetest.get_meta(pos)
+				node_meta:set_string("infotext", S("owner: ") .. player_name)
 			end
 			if not minetest.is_creative_enabled(player_name) then
 				itemstack:take_item()
